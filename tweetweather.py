a41f914b12307f4a1d98c4b7cfd66a2eeb26251a
Alex Solomon <alex.clinton.solomon@gmail.com>
2022-04-06 12:03:54 -0700

    streaming to file

--- tweetweather.py
+++ tweetweather.py
@@ -1,8 +1,7 @@
 
 # TODO use the more straightforward has:geo tag in when upgraded from Sandbox developer level- link to code
 # {"value": "twitter data has:geo"}
-
-
+import os
 from datetime import datetime
 import json
 import requests as requests
@@ -14,6 +13,12 @@ WEATHER_API_KEY = "991db1f58950453daea225235222803"
 TWITTER_BASE_URL = "https://api.twitter.com"
 TWITTER_SAMPLE_PATH = "/2/tweets/sample/stream?expansions=geo.place_id"
 TWITTER_LOCATION_PATH = "/2/tweets/"
+STREAM_OUTPUT_FILE = "data/tweet_weather_stream.txt"
+AVERAGE_OUTPUT_FILE = "data/tweet_weather_average.txt"
+
+# todo implement a real cache like redis, or persist to DB to avoid memory exceptions
+CACHE_OF_TEMPS = {}
+
 
 
 
@@ -75,42 +80,126 @@ class Integrations:
         r.headers["User-Agent"] = "v2TweetLookupPython"
         return r
 
-    def stream_twitter_locations(self):
+    def stream_twitter_locations(self, k):
 
-        # open to tweet sample stream
-        response = requests.get(
-            TWITTER_BASE_URL + TWITTER_SAMPLE_PATH,
-            # auth=self.twitter_bearer_oauth(auth_type="v2FilteredStreamPython", r = None),
-            auth=self.twitter_bearer_oauth,
-            stream=True,
-        )
-        print(response.status_code)
-        if response.status_code != 200:
-            raise Exception(
-                "Cannot get stream (HTTP {}): {}".format(
-                    response.status_code, response.text
+
+
+        # clean up an existing file
+        if os.path.exists(STREAM_OUTPUT_FILE):
+            os.remove(STREAM_OUTPUT_FILE)
+
+        # # create a new one
+        # os.create(STREAM_OUTPUT_FILE)
+
+        # Open a file in read/write mode
+        with open(STREAM_OUTPUT_FILE, "w") as writer_stream:
+            with open(AVERAGE_OUTPUT_FILE, "w") as writer_avg:
+                # write the headers for the output files
+
+                # open to tweet sample stream
+                response = requests.get(
+                    TWITTER_BASE_URL + TWITTER_SAMPLE_PATH,
+                    auth=self.twitter_bearer_oauth,
+                    stream=True,
                 )
-            )
-
-        # iterate over stream of results from twitter feed to filter those with a location only
-        for response_line in response.iter_lines():
-            if response_line:
-                json_response = json.loads(response_line)
-
-                # verify that returned data has a geotag location
-                if json_response['data']['geo'] != {}:
-                    # print(json.dumps(json_response, indent=4, sort_keys=True))
-                    tweet_id = json_response['data']['id']
-
-                    enrichment_request_url = f"https://api.twitter.com/2/tweets/{tweet_id}?expansions=geo.place_id&place.fields=full_name,geo,id,place_type"
-
-                    # enrichment_request_url = TWITTER_BASE_URL + TWITTER_LOCATION_PATH + f':{place_id}.json'
-                    enrichment_response = requests.get(
-                        enrichment_request_url,
-                        auth=self.twitter_bearer_oauth_tweet,
+                print(response.status_code)
+                if response.status_code != 200:
+                    raise Exception(
+                        "Cannot get stream (HTTP {}): {}".format(
+                            response.status_code, response.text
+                        )
                     )
 
-                    print(json.loads(enrichment_response.text))
+                # iterate over stream of results from twitter feed to filter those with a location only
+                for response_line in response.iter_lines():
+                    if response_line:
+                        json_response = json.loads(response_line)
+
+                        # verify that returned data has a geotag location
+                        if json_response['data']['geo'] != {}:
+                            tweet_id = json_response['data']['id']
+
+                            # todo: future improvement: can we batch our enrichment requests
+                            # todo clean up this request
+                            enrichment_request_url = f"https://api.twitter.com/2/tweets/{tweet_id}?expansions=geo.place_id&place.fields=full_name,geo,id,place_type"
+
+                            enrichment_response = requests.get(
+                                enrichment_request_url,
+                                auth=self.twitter_bearer_oauth_tweet,
+                            )
+
+                            # todo future improvement: gracefully handle some failures, and continue with new requests.
+                            if enrichment_response.status_code != 200:
+                                raise Exception(
+                                    "Request returned an error: {} {}".format(
+                                        response.status_code, response.text
+                                    )
+                                )
+
+                            enriched_tweet = json.loads(enrichment_response.text)
+                            tweet_geo = enriched_tweet["includes"]["places"][0]["geo"]
+
+
+                            # does this tweet have point or a bbox
+                            if tweet_geo["type"] == "Feature":
+                                bbox_coords = tweet_geo["bbox"]
+
+                                # get the centroid
+                                long = round((bbox_coords[0]+bbox_coords[2])/2,7)
+                                lat = round((bbox_coords[1]+bbox_coords[3])/2,7)
+
+                                print(long, lat)
+
+                            elif tweet_geo["type"] == "Point":
+
+                                # format for return is [Longigtude, Latitude]
+                                [long, lat] = tweet_geo["Coordinates"]
+
+                            # todo future work support a polygon object or V1 BBOX
+                            else:
+                                raise Exception(
+                                    "Request geo type is not supported"
+                                    )
+
+                            # call the weather API
+                            weather = self.get_weather(lat=lat, long=long)
+
+                            # create result line for streaming file
+                            line_weather_stream = f"Tweet Location: {weather.location} - {weather.country},"\
+                                    + f"temp_f: {weather.temperature_f}, last_updated: {weather.last_updated}\n"
+
+                            # Write a line at the end of the file.
+                            writer_stream.write(line_weather_stream)
+
+                            # update the cache
+                            if weather.location in CACHE_OF_TEMPS:
+                                temps_array = CACHE_OF_TEMPS[weather.location]
+
+                                # bump a temp if we are at the max
+                                if len(temps_array) == k:
+                                    temps_array.pop(0)
+
+                                # add the newest temp to the other end
+                                temps_array.append(weather.temperature_f)
+
+                                # update the cache
+                                CACHE_OF_TEMPS[weather.location] = temps_array
+                            else:
+                                # if location not already in the cahce, initialize it with a new array
+                                temps_array = [weather.temperature_f]
+                                CACHE_OF_TEMPS[weather.location] = temps_array
+
+                            number_of_data_points = len(temps_array)
+                            average_temp = sum(temps_array)/len(temps_array)
+
+                            line_weather_average = f"Tweet Location: {weather.location} - {weather.country},"\
+                                    + f"rolling_avg_temp_f: {average_temp}, last_updated: {weather.last_updated}, "\
+                                    + f"number_of_data_points: {number_of_data_points}\n"
+
+
+
+                            # write rolling average value cache to streaming file 2
+                            writer_avg.write(line_weather_average)
 
 
 def main():
@@ -119,7 +208,7 @@ def main():
 
     # start the stream of sample tweets
     integrations = Integrations()
-    integrations.stream_twitter_locations()
+    integrations.stream_twitter_locations(k=k)
 
 if __name__ == "__main__":
     main()
